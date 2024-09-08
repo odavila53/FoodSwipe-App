e343781b5107980e70d9c4c1cd520653728bd35b
odavila53 <odavila6729@gmail.com>
2024-09-08 13:47:18 -0700

    Initial Commit

---
+++ IndecisiveFood/IndecisiveFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndecisiveFoodApp.swift
+//  IndecisiveFood
+//
+//  Created by Oliver Davila on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct IndecisiveFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
